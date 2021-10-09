d4524cede30e2d8af136f10e5bade607bfeb1e5d
Ben Cobley <32883278+bencobley@users.noreply.github.com>
2021-10-09 19:42:40 +0100

    Import album art from url

--- resize.py
+++ resize.py
@@ -1,8 +1,13 @@
 from PIL import Image
+import urllib.request
 
-img = Image.open('cover.jpg')
+urllib.request.urlretrieve(
+  'https://i.scdn.co/image/ab67616d0000b27357751ae9890e997676b998f2',
+   "cover.jpeg")
+
+img = Image.open('cover.jpeg')
 img = img.resize((448, 448), Image.ANTIALIAS)
 
 img_resized = Image.new(img.mode, (600, 448), (255, 255, 255))  
 img_resized.paste(img, (76, 0))
-img_resized.save('display.jpg')
+img_resized.save('display.jpeg')
